752639a907c26bf79cad5c5abc52abbd65f90a2c
Mikhail Chugunkov <poslegm@gmail.com>
2021-06-06 23:24:14 +0300

    DI over test local fixture spec

---
+++ src/test/scala/munit/ZLayersSpec.scala
@@ -0,0 +1,65 @@
+package munit
+
+import zio.*
+
+import SampleDependencies.*
+
+class ZLayerSpec extends ZSuite:
+  test("simple layers providing") {
+    val deps = ZLayer.succeed(A()) >>> ZLayer.fromService[A, B](B(_))
+
+    val effect = ZIO.service[B].map(_.g)
+    assertZ(effect.provideLayer(deps))
+  }
+
+  val layersFixture = ZTestLocalFixture { _ =>
+    ZManaged.make(ZIO.succeed(StatefulRepository.test >+> Service.test))(layers =>
+      clean.provideLayer(layers)
+    )
+  }
+
+  layersFixture.test("fixture layers providing") { layers =>
+    val effect = write *> write *> fetch
+    assertEqualsZ(effect.provideLayer(layers), 2)
+  }
+
+  layersFixture.test("fixture layers providing after clean") { layers =>
+    val effect = write *> write *> fetch
+    assertEqualsZ(effect.provideLayer(layers), 2)
+  }
+
+object SampleDependencies:
+  // stateless
+  class A:
+    def f: Boolean = false
+
+  class B(a: A):
+    export a.*
+    def g: Boolean = !f
+  // ======
+
+  // stateful
+  class StatefulRepository:
+    private var state = 0
+
+    def getState: Task[Int] = ZIO.effectTotal { state }
+
+    def inc: Task[Unit] =
+      ZIO.effectTotal { state += 1 }
+
+    def clean: UIO[Unit] =
+      ZIO.effectTotal { state = 0 }
+
+  object StatefulRepository:
+    val test = ZLayer.succeed(StatefulRepository())
+
+  class Service(repo: StatefulRepository):
+    def fetch: Task[Int]  = repo.getState
+    def write: Task[Unit] = repo.inc
+
+  object Service:
+    val test = ZLayer.fromService[StatefulRepository, Service](Service(_))
+
+  def clean = ZIO.service[StatefulRepository].flatMap(_.clean)
+  def fetch = ZIO.service[Service].flatMap(_.fetch)
+  def write = ZIO.service[Service].flatMap(_.write)
--- src/test/scala/munit/ZTestLocalFixturesSpec.scala
+++ src/test/scala/munit/ZTestLocalFixturesSpec.scala
@@ -3,41 +3,7 @@ package munit
 import zio.*
 import zio.console.*
 
-import SampleDependencies.*
-
-// TODO simple resource management (raw ZManaged) TEST-LOCAL DONE
-// TODO resource management on layers (init/clean)
-
-// FunFixture with layer. syntax:
-// layers.testZIO("") {
-//   ... accepts RIO[Layers, Any]
-// }
-//
-//
-// resource test/suite local fixture with ZManaged (like in munit-cats-effect). syntax:
-//
-// suite local:
-// val fixture = ZManagedFixture.make(...)(...)
-// test("suite local") {
-//   ... accepts fixture call for resource
-// }
-//
-// test local:
-// val fixture = ZManagedFixture.make(...)(...)
-// fixture.testZ("") { resource =>
-//   ...
-// }
-
-// TODO fixtures should be composed with map2 for layers/resources composition
-
-class ZFixturesSpec extends ZSuite:
-  test("simple layers providing") {
-    val deps = ZLayer.succeed(A()) >>> ZLayer.fromService[A, B](B(_))
-
-    val effect = ZIO.service[B].map(_.g)
-    assertZ(effect.provideLayer(deps))
-  }
-
+class ZTestLocalFixturesSpec extends ZSuite:
   val rawZIOFunFixture = ZTestLocalFixture(options => ZIO.succeed(s"acquired ${options.name}")) {
     str =>
       putStrLn(s"cleanup [$str]").provideLayer(Console.live)
@@ -62,11 +28,3 @@ class ZFixturesSpec extends ZSuite:
       assertNoDiff(str1, "acquired compose ZIO FunFixtures")
       assertNoDiff(str2, "acquired compose ZIO FunFixtures with ZManaged")
   }
-
-object SampleDependencies:
-  class A:
-    def f: Boolean = false
-
-  class B(a: A):
-    export a.*
-    def g: Boolean = !f
