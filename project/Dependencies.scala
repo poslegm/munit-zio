e9ff8af63ffc0f6b5446c0c8386458582221fa26
Mikhail Chugunkov <poslegm@gmail.com>
2021-08-15 17:00:21 +0300

    Update munit to 0.7.28 (#14)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Dependencies {
-  private val munitVersion = "0.7.27"
+  private val munitVersion = "0.7.28"
   private val zioVersion   = "1.0.10"
 
   lazy val munit = "org.scalameta" %% "munit" % munitVersion
