055d61fee938933785b35e67be1c1f443e7b5f98
Mikhail Chugunkov <poslegm@gmail.com>
2021-08-15 16:56:25 +0300

    Update zio to 1.0.10 (#13)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   private val munitVersion = "0.7.27"
-  private val zioVersion   = "1.0.9"
+  private val zioVersion   = "1.0.10"
 
   lazy val munit = "org.scalameta" %% "munit" % munitVersion
   lazy val zio   = "dev.zio"       %% "zio"   % zioVersion
